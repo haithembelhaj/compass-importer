4799b12830b6260f5f840dbaeee17dd3b9b1b410
Haithem Bel Haj <haythem.belhaj@gmail.com>
2015-09-23 19:08:03 +0200

    make directory configurable

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-module.exports = function(url, prev, done) {
+var resolve = require('path').resolve;
+
+module.exports = function(path, prev, done) {
 
     var request, fs, oldHeaders;
+    var self = this;
     var githubUrl = 'https://github.com/Igosuki/compass-mixins/archive/master.zip';
 
-    if (!/^compass/.test(url))
-        return done({file: url});
+    this.cacheDirectory = this.cacheDirectory || resolve(__dirname + '/.compass');
+
+    var file = resolve(this.cacheDirectory + '/compass-mixins-master/lib/' + path);
+    var headerFile = resolve(this.cacheDirectory  + '/headers.json');
+
+    if (!/^compass/.test(path))
+        return done({file: path});
 
     fs = require('fs-extra');
     request = require('request');
 
 
-    if (!fs.existsSync('.compass/headers.json'))
+    if (!fs.existsSync(headerFile))
         return download();
 
-    done({file: 'compass-mixins-master/lib/' + url});
+    done({file: file});
 
-    oldHeaders = fs.readJsonSync('.compass/headers.json');
+    oldHeaders = fs.readJsonSync(headerFile);
 
     console.log('verify compass version ...');
 
@@ -35,14 +43,14 @@ module.exports = function(url, prev, done) {
         request
             .get(githubUrl)
             .on('response', function (res) {
-                fs.writeJsonSync('.compass/headers.json', res.headers);
+                fs.writeJsonSync(headerFile, res.headers);
             })
-            .pipe(unzip.Extract({path: '.compass'}))
+            .pipe(unzip.Extract({path: self.cacheDirectory}))
             .on('close', function () {
 
                 console.log('done');
 
-                done({file: 'compass-mixins-master/lib/' + url})
+                done({file: file})
             });
     }
 }
