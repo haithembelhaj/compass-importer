5560296e2737b780c605117b74d014f3fa41b468
Daniil Tomilow <daniil.tomilow@gmail.com>
2015-10-16 14:09:32 +0200

    Import compass animation as well
     * test for animation
     * gulp example code to readme
     * travis ci

--- test/index.js
+++ test/index.js
@@ -21,4 +21,21 @@ describe('test importer', function(){
 
   })
 
+  it('should convert css3 animations', function(done){
+
+    this.timeout(10000);
+
+    sass.render({
+      data: '@import "compass"; @import "compass/css3/animation"; @include keyframes(test) { from {background-color: red;} to {background-color: yellow; } } .animation { @include animation(test 1s); }',
+      importer: compass,
+      outputStyle: 'compressed'
+    }, function(err, result){
+      console.log(err);
+
+      expect(result.css.toString()).to.equal('@-moz-keyframes test{from{background-color:red}to{background-color:yellow}}@-webkit-keyframes test{from{background-color:red}to{background-color:yellow}}@-o-keyframes test{from{background-color:red}to{background-color:yellow}}@-ms-keyframes test{from{background-color:red}to{background-color:yellow}}@keyframes test{from{background-color:red}to{background-color:yellow}}.animation{-webkit-animation:test 1s;-moz-animation:test 1s;-ms-animation:test 1s;-o-animation:test 1s;animation:test 1s}\n');
+      done(err);
+    });
+
+  })
+
 });
