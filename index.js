247115cb69afe2382c15f06991371f66d6a1ddb4
Haithem Bel Haj <haythem.belhaj@gmail.com>
2016-02-09 10:38:37 +0100

    using resolve for compass module module resolution

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 // imports
-var fs = require('fs-extra');
-var request = require('request');
-var unzip = require('unzip');
 var resolve = require('path').resolve;
+var sync = require('resolve').sync;
 
-// vars
-var githubUrl = 'https://github.com/Igosuki/compass-mixins/archive/master.zip';
-var importRegexp = /^compass|^animation/;
+var compassPath = sync('compass-mixins');
+var libPath = resolve(compassPath, '..');
 
-// dirs and files
-var cacheDirectory = resolve(__dirname + '/.compass');
-var libPath = resolve(cacheDirectory + '/compass-mixins-master/lib/');
-var headerFile = resolve(cacheDirectory  + '/headers.json');
-
-var headers = fs.existsSync(headerFile) && fs.readJsonSync(headerFile);
-
-var safeUpdate = once(update);
+var compassRegexp = /^compass|^animation/;
 
 // exports
 module.exports = importer;
@@ -32,80 +22,8 @@ module.exports = importer;
  */
 function importer(path, _, done) {
 
-  var file = resolve(libPath +'/'+ path);
-
-  if (!importRegexp.test(path))
+  if (!compassRegexp.test(path))
     return done({file: path});
 
-  if (!headers)
-    return download(callback);
-
-  safeUpdate();
-  callback();
-
-  function callback(){
-
-    done({file: file})
-  }
-}
-
-/**
- * update the installed version of compass only if not uptodate
- * @param callback
- */
-function update(callback){
-
-  request.head(githubUrl).on('response', function (res) {
-
-    console.log('verifying compass ...');
-
-    if(headers.etag !== res.headers.etag)
-      download(callback)
-  });
-}
-
-/**
- * downolad the latest version on compass
- * @param callback
- */
-function download(callback) {
-
-  console.log('downloading compass ...');
-
-  request
-    .get(githubUrl)
-    .on('error', console.log.bind(console))
-    .on('response', function (res) {
-
-      headers = res.headers;
-
-      fs.writeJson(headerFile, res.headers);
-    })
-    .pipe(unzip.Extract({path: cacheDirectory}))
-    .on('close', function () {
-
-      console.log('latest version installed');
-
-      if(callback) callback();
-    });
-}
-
-// execute a function only if it's not in progress
-function once(fn){
-
-  var progress = false;
-
-  return function(callback){
-
-    if(progress) return;
-
-    progress = true;
-
-    fn(function(){
-
-      progress = false;
-
-      if(callback) callback();
-    })
-  }
+  return done({file: resolve(libPath,  path)})
 }
